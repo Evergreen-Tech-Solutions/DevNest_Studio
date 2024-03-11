af4527c8361623728ed8ea63b3096f3c33ac5d88
sara-jfi <sarajafari@uvic.ca>
2024-03-11 13:19:31 -0700

    Using M-UI to make the Dark mode

--- src/App.js
+++ src/App.js
@@ -1,42 +1,69 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
-
-
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ServicesPage from './pages/ServicesPage';
 import ProjectsPage from './pages/ProjectsPage';
 import ContactPage from './pages/ContactPage';
 
+
+
 function App() {
 
+  // const [darkMode, setDarkMode] = useState(false);
+  // const toggleDarkMode = () => setDarkMode(!darkMode);
+
+  // const [colorBlindMode, setColorBlindMode] = useState(false);
+  // const toggleColorBlindMode = () => setColorBlindMode(!colorBlindMode);
+
+  // useEffect(() => {
+  //   if (darkMode) {
+  //     document.documentElement.classList.add('dark-mode');
+  //   } else {
+  //     document.documentElement.classList.remove('dark-mode');
+  //   }
+  // }, [darkMode]);
+
+  // useEffect(() => {
+  //   if (colorBlindMode) {
+  //     document.documentElement.classList.add('color-blind');
+  //   } else {
+  //     document.documentElement.classList.remove('color-blind');
+  //   }
+  // }, [colorBlindMode]);
+
+
   const [darkMode, setDarkMode] = useState(false);
-  const toggleDarkMode = () => setDarkMode(!darkMode);
 
-  useEffect(() => {
-    if (darkMode) {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-  }, [darkMode]);
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      // You can customize other theme properties here
+      // customize the colors here
+      primary: {
+        main: '#b6e1e0',
+      },
+      secondary: {
+        main: '#3f3f3f',
+      },
+    },
+  });
 
-  const [colorBlindMode, setColorBlindMode] = useState(false);
-  const toggleColorBlindMode = () => setColorBlindMode(!colorBlindMode);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
-  useEffect(() => {
-    if (colorBlindMode) {
-      document.body.classList.add('color-blind-mode');
-    } else {
-      document.body.classList.remove('color-blind-mode');
-    }
-  }, [colorBlindMode]);
+
+  
   return (
     <Router>
-      <div className={`App flex flex-col min-h-screen ${darkMode ? 'dark' : 'light'} ${colorBlindMode ? 'color-blind' : ''}`}>
-      <Navigation onToggleDarkMode={toggleDarkMode} onToggleColorBlindMode={toggleColorBlindMode} />
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App flex flex-col min-h-screen">
+      <Navigation onToggleDarkMode={toggleDarkMode} />
           <main className="flex-1 pt-[64px]">
             <Routes>
               <Route path="/" element={<HomePage />} />
@@ -47,6 +74,7 @@ function App() {
             </Routes>
           </main>
         </div>
+        </ThemeProvider>
     </Router>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,43 +1,91 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../tailwind.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu'; // Hamburger menu icon
+import Brightness4Icon from '@mui/icons-material/Brightness4'; // Dark mode icon
+import Brightness7Icon from '@mui/icons-material/Brightness7'; // Light mode icon
 import logo from '../assets/logo.png';
+import '../tailwind.css';
 import style from '../styles/Navigation.module.css';
 
-
-function Navigation({ darkMode, toggleDarkMode, colorBlindMode, toggleColorBlindMode }) {
+function Navigation({ onToggleDarkMode, darkMode }) {
   return (
-    <nav className="fixed top-0 left-0 w-full z-10 bg-blu py-4 shadow-md">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-              <NavLink to="/">
-                <img src={logo} alt="DevNest Studio Logo" className="h-24 w-24" />
-              </NavLink>
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline">
-                <NavLink to="/" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Home</NavLink>
-                <NavLink to="/about" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>About</NavLink>
-                <NavLink to="/services" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Services</NavLink>
-                <NavLink to="/projects" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Projects</NavLink>
-                <NavLink to="/contact" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Contact</NavLink>
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center space-x-4">
-            <button onClick={toggleDarkMode} className="p-2 rounded">
-              {darkMode ? <i className="fas fa-sun"></i> : <i className="fas fa-moon"></i>}
-            </button>
-            <button onClick={toggleColorBlindMode} className="p-2 rounded">
-              {colorBlindMode ? <i className="fas fa-eye"></i> : <i className="fas fa-low-vision"></i>}
-            </button>
-          </div>
+    <AppBar position="fixed">
+      <Toolbar className="w-full z-10">
+        <NavLink to="/" className="flex items-center">
+          <img src={logo} alt="DevNest Studio Logo" className="h-28 w-28 mr-2" />
+          <Typography variant="h6" color="inherit" noWrap>
+          </Typography>
+        </NavLink>
+        <div className="flex-grow"></div>
+        <div className="ml-12 flex items-baseline align-middle">
+          <NavLink to="/" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Home</NavLink>
+          <NavLink to="/about" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>About</NavLink>
+          <NavLink to="/services" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Services</NavLink>
+          <NavLink to="/projects" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Projects</NavLink>
+          <NavLink to="/contact" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Contact</NavLink>
         </div>
-      </div>
-    </nav>
+
+        {/* Dark Mode Toggle Button */}
+        <IconButton color="inherit" onClick={onToggleDarkMode}>
+          {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 }
 
 export default Navigation;
+
+
+
+
+
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import '../tailwind.css';
+// import logo from '../assets/logo.png';
+// import style from '../styles/Navigation.module.css';
+// import '../App.css';
+
+
+
+// function Navigation({ onToggleDarkMode }) {
+//   return (
+//     <nav className={`fixed top-0 left-0 w-full z-10 bg-blu py-4 shadow-xl ${darkMode ? 'dark-mode' : 'light-mode'}`}>
+//       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+//         <div className="flex items-center justify-between h-16">
+//           <div className="flex items-center">
+//             <div className="flex-shrink-0">
+//               <NavLink to="/">
+//                 <img src={logo} alt="DevNest Studio Logo" className="h-24 w-24" />
+//               </NavLink>
+//             </div>
+//             <div className="hidden md:block">
+//               <div className="ml-12 flex items-baseline">
+//                 <NavLink to="/" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Home</NavLink>
+//                 <NavLink to="/about" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>About</NavLink>
+//                 <NavLink to="/services" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Services</NavLink>
+//                 <NavLink to="/projects" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Projects</NavLink>
+//                 <NavLink to="/contact" className={({ isActive }) => isActive ? style.navItemActive : style.navItemInactive}>Contact</NavLink>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="flex items-center space-x-4">
+//             <button onClick={toggleDarkMode} className="p-2 rounded">
+//               {darkMode ? <i className="fas fa-sun"></i> : <i className="fas fa-moon"></i>}
+//             </button>
+//             <button onClick={toggleColorBlindMode} className="p-2 rounded">
+//               {colorBlindMode ? <i className="fas fa-eye"></i> : <i className="fas fa-low-vision"></i>}
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navigation;
